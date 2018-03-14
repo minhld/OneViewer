065284eba71b51d3b2bb3a75c236274783251deb
minhld <ledinhminh3883@gmail.com>
2018-03-13 18:54:06 -0600

    added side menu (on-going)

--- app/src/main/java/com/usu/oneviewer/MainActivity.java
+++ app/src/main/java/com/usu/oneviewer/MainActivity.java
@@ -1,13 +1,33 @@
 package com.usu.oneviewer;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.item1) {
+
+        } else if (id == R.id.item2) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
