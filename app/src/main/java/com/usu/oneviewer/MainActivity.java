5a9856f104524141cc4f5f0d9d5a2ba0d4fd7107
minhld <ledinhminh3883@gmail.com>
2018-04-16 23:59:59 -0600

    adding some icons for side menu

--- app/src/main/java/com/usu/oneviewer/MainActivity.java
+++ app/src/main/java/com/usu/oneviewer/MainActivity.java
@@ -106,13 +106,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.item1) {
+        if (id == R.id.mainItem) {
 
-        } else if (id == R.id.item2) {
+        } else if (id == R.id.manageItem) {
+
+        } else if (id == R.id.settingsItem) {
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
